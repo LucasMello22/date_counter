ea207015e98b0bea84ba9de4ab0d9c4076eafa02
Lucas Mello22 <lucas.s.mello@outlook.com>
2024-05-08 14:14:38 -0300

    version two, modified to work on another device

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,160 +1,140 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+import '../main.dart';
+
+
+
+class HomePage extends StatelessWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora de Diferença de Datas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DateDifferencePage(),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
-  late DateTime _dataInicial;
-  late DateTime _dataFinal;
-  late int _quantidadeDias;
+class DateDifferencePage extends StatefulWidget {
+  @override
+  _DateDifferencePageState createState() => _DateDifferencePageState();
+}
+
+class _DateDifferencePageState extends State<DateDifferencePage> {
+  late DateTime _startDate;
+  late DateTime _endDate;
+  int _differenceDays = 0;
 
   @override
   void initState() {
     super.initState();
-    _dataInicial = DateTime(2022, 12, 23);
-    _dataFinal = DateTime(2023, 12, 18);
-    _calcularQuantidadeDias();
+    // Inicializando as datas com a data atual
+    _startDate = DateTime.now();
+    _endDate = DateTime.now();
+    // Calculando a diferença inicialmente
+    _calculateDifference();
   }
 
-  void _calcularQuantidadeDias() {
+  void _calculateDifference() {
+    // Calculando a diferença em dias
+    final difference = _endDate.difference(_startDate).inDays;
     setState(() {
-      _quantidadeDias = _dataFinal.difference(_dataInicial).inDays;
+      _differenceDays = difference.abs();
     });
+  }
+
+  Future<void> _selectStartDate(BuildContext context) async {
+    final pickedStartDate = await showDatePicker(
+      context: context,
+      initialDate: _startDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedStartDate != null && pickedStartDate != _startDate) {
+      setState(() {
+        _startDate = pickedStartDate;
+      });
+      _calculateDifference();
+    }
+  }
+
+  Future<void> _selectEndDate(BuildContext context) async {
+    final pickedEndDate = await showDatePicker(
+      context: context,
+      initialDate: _endDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedEndDate != null && pickedEndDate != _endDate) {
+      setState(() {
+        _endDate = pickedEndDate;
+      });
+      _calculateDifference();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFB9CCF2),
-      body: Stack(
-        children: [
-          Positioned(
-            left: -60,
-            top: 650,
-            child: CustomPaint(
-              size: Size(300, 300),
-              painter: EllipsePainter(color: Color.fromRGBO(10, 72, 233, 0.42)),
-            ),
-          ),
-          Positioned(
-            left: 206.02,
-            top: -116,
-            child: CustomPaint(
-              size: Size(300, 300),
-              painter: EllipsePainter(color: Color.fromRGBO(10, 72, 233, 0.42)),
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Data Inicial:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    '${_dataInicial.day}/${_dataInicial.month}/${_dataInicial.year}',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Data Final:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    '${_dataFinal.day}/${_dataFinal.month}/${_dataFinal.year}',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Quantidade de Dias:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    _quantidadeDias.toString(),
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      _mostrarDatePickerInicial(context);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text('Selecionar Data Inicial'),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      _mostrarDatePickerFinal(context);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text('Selecionar Data Final'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+      body:
+      Stack(
+     children: [
+       Positioned(
+        left: -60,
+        top: 650,
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: EllipsePainter(color: Color.fromRGBO(10, 72, 233, 0.42)),
+        ),
       ),
-    );
-  }
+      Positioned(
+        left: 206.02,
+        top: -116,
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: EllipsePainter(color: Color.fromRGBO(10, 72, 233, 0.42)),
+        ),
+      ),
 
-  void _mostrarDatePickerInicial(BuildContext context) async {
-    final DateTime? novaDataInicial = await showDatePicker(
-      context: context,
-      initialDate: _dataInicial,
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
+          Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Data de Início: ${_startDate.day}/${_startDate.month}/${_startDate.year}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _selectStartDate(context),
+              child: Text('Selecionar Data de Início'),
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Data de Fim: ${_endDate.day}/${_endDate.month}/${_endDate.year}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _selectEndDate(context),
+              child: Text('Selecionar Data de Fim'),
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Diferença em Dias: $_differenceDays',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+     ],),
     );
-    if (novaDataInicial != null && novaDataInicial != _dataInicial) {
-      setState(() {
-        _dataInicial = novaDataInicial;
-        _calcularQuantidadeDias();
-      });
-    }
-  }
 
-  void _mostrarDatePickerFinal(BuildContext context) async {
-    final DateTime? novaDataFinal = await showDatePicker(
-      context: context,
-      initialDate: _dataFinal,
-      firstDate: _dataInicial,
-      lastDate: DateTime.now(),
-    );
-    if (novaDataFinal != null && novaDataFinal != _dataFinal) {
-      setState(() {
-        _dataFinal = novaDataFinal;
-        _calcularQuantidadeDias();
-      });
-    }
   }
 }
-
-class EllipsePainter extends CustomPainter {
-  final Color color;
-
-  EllipsePainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint()..color = color;
-
-    final Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
-    canvas.drawOval(rect, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-
